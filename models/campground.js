1a4afa490f34af0e50353342129e10935d038ef2
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-10 11:31:35 -0400

    Added basic review model.

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,11 @@ const CampgroundSchema = new Schema ({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
