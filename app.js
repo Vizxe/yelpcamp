150481c021809776f3f7db98f830e27074d3285b
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-10 14:16:50 -0400

    Added delete button to reviews

--- app.js
+++ app.js
@@ -81,6 +81,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync (async (req,res)
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const {id, reviewId} = req.params;
+    Campground.findByIdAndUpdate(id,  {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+})) 
+
 app.get('/campgrounds/:id/edit', catchAsync (async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
