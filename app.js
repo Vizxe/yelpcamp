fa9318aeb1e4afc98eef802b83eb030a86626119
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-14 19:31:00 -0400

    Added session cookies
    Using local memory for now

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const session = require('express-session')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -25,6 +26,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+const sessionConfig = {
+    secret: 'WOoaHwHatAGREATsecrRET',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 
 app.use('/campgrounds', campgrounds)
