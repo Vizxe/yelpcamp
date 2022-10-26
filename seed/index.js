2479bd74e5951a997ba2d148d298548b41a4fb3e
Parsa Hemmati <parsahemmati20@gmail.com>
2022-10-26 17:18:25 -0400

    updated seeding

--- seed/index.js
+++ seed/index.js
@@ -21,7 +21,16 @@ const seedDB = async() => {
             author: '6355933e90c1aad59683f4d5',
             location: `${sample(cities).city}, ${sample(cities).state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dcm1co1r4/image/upload/v1666818858/YelpCamp/we68xj7u8fswfk9qxhr8.png',
+                  filename: 'YelpCamp/we68xj7u8fswfk9qxhr8',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dcm1co1r4/image/upload/v1666818860/YelpCamp/wlp3es25ba4yihdelfaj.png',
+                  filename: 'YelpCamp/wlp3es25ba4yihdelfaj',
+                }
+              ],
             description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe nostrum accusantium sequi molestias rerum amet aliquam voluptatibus id, tempore magni veritatis!",
             price: price
         })
