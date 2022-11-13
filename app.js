19994e1f0f9a91c94efeb930429d90e5e3a65279
Parsa Hemmati <parsahemmati20@gmail.com>
2022-11-13 13:34:58 -0500

    added start script

--- app.js
+++ app.js
@@ -147,7 +147,8 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Oops! Something went wrong.";
     res.status(statusCode).render('partials/error', {err})
 });
- 
-app.listen(3000, ()=> {
-    console.log('Yoo someone activated port 3000');
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log(`Yoo someone activated port ${port}`);
 })
